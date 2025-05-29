7473933b04975d57e25b51058fc0742eff491f6f
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-05-29 12:12:59 +0000

    exercicio01.py

--- exercicio01.py
+++ exercicio01.py
@@ -3,4 +3,13 @@
 
 # Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
 # Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
+curso01 = int(input("Digite a quantidade de acessos do curso01"))
+curso02 = int(input("Digite a quantidade de acessos do curso02"))
+
+if curso01 > curso02 :
+    print("Curso01 tem mais acessos.")
+elif curso02 > curso01 :
+    print("Curso02 tem mais acessos.")
+else:
+    print("Ambos os cursos tem a mesma quantidade de acessos.")
 
