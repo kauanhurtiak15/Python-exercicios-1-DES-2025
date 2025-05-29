532f318350af16ce0563cd9132b3558ab458bedc
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-05-29 14:38:51 +0000

    exercicio02.py

--- exercicio02.py
+++ exercicio02.py
@@ -2,4 +2,11 @@
 # para finalizar três tarefas: X, Y e Z.
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
+trabalhoX = int(input("Digite o tempo do trabalhoX"))
+trabalhoY = int(input("Digite o tempo do trabalhoY"))
+trabalhoZ = int(input("Digite o tempo do trabalhoZ"))
 
+if any > 0 :
+    print("Entregue à tempo.")
+elif any < 0 :
+    print("Não entregue à tempo.")
