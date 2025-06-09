07e8ab2a7b870606cb3addd5815f9932ac694418
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-06-09 14:14:10 +0000

    vetor.py

--- exercicio03.py
+++ exercicio03.py
@@ -2,10 +2,3 @@
 # Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
 
 umidade = int(input("Digite a umidade local"))
-
-umidade = 20
-
-if umidade >= 20:
-    print("umidade normal.")
-elif umidade > 20:
-    print("A umidade ultrapassou o limite.")
--- exercicio06.py
+++ exercicio06.py
@@ -1,3 +1,8 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
+plataforma = int(input("Digite que horas você quer acessar a plataforma: "))
 
+if plataforma < 9:
+    print("Acesso negado.")
+elif plataforma > 9:
+    print("Acesso permitido.")
