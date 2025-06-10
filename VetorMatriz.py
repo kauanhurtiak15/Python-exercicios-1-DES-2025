07914a19e0f6f8906f1c69b33df05c7be7b74870
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-06-10 14:01:41 +0000

    VetorMatriz.py finalizado

--- VetorMatriz.py
+++ VetorMatriz.py
@@ -11,4 +11,6 @@ for i, aluno in enumerate (alunos) :
             reservas[i] [i] = "Presente"
 
 print("\nTabela de reservas:")
-print(f"{'Aluno':<10} 
+print(f"{'Aluno':<10}{''.join([f'{d:<10}'for d in dias])}")
+for i, aluno in enumerate (alunos):
+    print{f"{aluno:<10} {''.join([f'{res:<10}'for res in reservas [i]])}"}
