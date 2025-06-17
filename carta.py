446fa02e09045a3bac7cc28c0da922c3ddd65d60
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-06-17 11:49:26 +0000

    carta.py terminado

---
+++ carta.py
@@ -0,0 +1,6 @@
+destinatario = input("Digite o nome do destinatário: ")
+mensagem = input("Digite sua mensagem: ")
+remetente = input("Digite seu nome: ")
+
+print("\n--- sua carta ---")
+print(f"Olá {destinatario},\n\n{mensagem}\n\nAtenciosamente,\n{remetente}")
