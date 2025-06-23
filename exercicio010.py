1d9b4f8e3ab6caffc87f5eb8993e832c67713ac2
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-06-23 14:03:50 +0000

    exercicio10.py terminado

--- exercicio010.py
+++ exercicio010.py
@@ -2,4 +2,10 @@
 
 # Salário mensal acima de R$ 3.000,00
 # A parcela não pode ser maior que 35% do salário
+saláriomensal = float(input("Digite o saláriomensal"))
+parcela = float(input("Digite a parcela"))
 
+if saláriomensal >= 3000 and parcela <= 35:
+    print("Seu financiamento foi solicitado.")
+else:
+    print("Os critérios não foram atendidos.")
