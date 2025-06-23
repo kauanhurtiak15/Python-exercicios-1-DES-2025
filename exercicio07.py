0a8e90ee5d74a312004f99776bbe9bcf426d32ce
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-06-23 13:44:18 +0000

    exercicio07.py terminado

--- exercicio07.py
+++ exercicio07.py
@@ -4,5 +4,19 @@
 # Aprovado (>=7)
 # Em treinamento (>=5 e <7)
 # Reprovado (<5)
+media1 = float(input("Digite a primeira média: "))
+media2 = float(input("Digite a segunda média: "))
+media3 = float(input("Digite a terceira média: "))
+
+media = media1 or media2 or media3
+
+print(f"media: {media:.2}")
+
+if media >= 7:
+    print("Aprovado.")
+elif media >= 5:
+    print("Em treinamento.")
+else:
+    print("reprovado.")
 
 
