d11f6c3b9a4738aaf3c28b17660a64a4b65587ec
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-06-24 12:03:32 +0000

    exercicio11.py terminado

--- exercicio11.py
+++ exercicio11.py
@@ -5,3 +5,14 @@
 #Peso normal(18.5 a 24.9)
 #Sobrepeso(25 a 29.9)
 #Obesidade(>=30)
+
+peso = float(input("Digite o peso da pessoa: "))
+altura = float(input("Digite a altura da pessoa: "))
+
+IMC = peso/altura
+if peso < 18.5:
+    print("Abaixo do peso.")
+elif peso <= 18.5 >= 24.9:
+    print("Peso normal.")
+else:
+    print("sobrepeso.")
