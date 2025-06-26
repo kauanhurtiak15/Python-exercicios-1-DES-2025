c2b9e7191445abf9389f9dfc146dbb1b53fbbedb
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-06-26 11:49:56 +0000

    texto.py terminado

---
+++ exercicio12.py
@@ -0,0 +1,5 @@
+#Peça ao usuário uma senha e verifique se ela contém pelo menos 8 caracteres.
+#Exiba uma mensagem de "Senha válida" ou "Senha muito curta".
+senha = int(input("digite uma senha: "))
+
+
---
+++ texto.py
@@ -0,0 +1,8 @@
+print("Olá, mundo!")
+
+nome = "Mailon"
+sobrenome = "Vitor"
+print("Nome  completo: " + nome + " " + sobrenome)
+
+idade = 16
+print(f"{nome} {sobrenome} tem {idade} anos.")
